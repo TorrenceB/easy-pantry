a8818f62555e3222a97f2ecf72303563560c31cc
Torrence Brown <torrenceb90@gmail.com>
2021-08-21 15:11:38 -0600

    Added in featured component

---
+++ src/ui/Featured.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container class="text-center">
+    <h2>Featured Recipe</h2>
+    <v-card>
+      <v-img
+        src="https://restaurantden.com/wp-content/uploads/2017/09/free-stock-food-photography-websites.jpg"
+      ></v-img>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Featured",
+};
+</script>
