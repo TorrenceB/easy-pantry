c9f856c83e274137dfe745ea7ef6c2f915b2d6fb
Torrence Brown <torrenceb90@gmail.com>
2021-09-21 17:55:14 -0600

    Encapsulated url functionality

--- src/util/fetchRecipeClient.js
+++ src/util/fetchRecipeClient.js
@@ -1,13 +1,24 @@
 const fetchRecipes = (url, params) => {
-  const recipeKey = process.env.VUE_APP_RECIPE_KEY;
-
-  return fetch(`${url}?apiKey=${recipeKey}&${params ?? ""}`, {
+  const options = {
     method: "GET",
     mode: "cors",
     headers: {
       "Content-Type": "application/json",
     },
-  })
+  };
+
+  const setURL = (url) => {
+    const recipeKey = process.env.VUE_APP_RECIPE_KEY;    
+    url = url += `?apiKey=${recipeKey}`;
+
+    if (params) {
+      url += `&${JSON.stringify(params)}`;
+    }
+
+    return url;
+  };
+
+  return fetch(setURL(url), options)
     .then((response) => {
       if (response.ok) {
         return response.json();
