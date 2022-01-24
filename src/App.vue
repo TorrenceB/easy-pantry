097fb91d79a981fc5c49928dc36547efc26146d6
Torrence Brown <torrenceb90@gmail.com>
2022-01-23 19:05:08 -0700

    Added sign out functionality

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <v-app>
-    <amplify-authenticator class="center">
+    <div v-if="!isSignedIn" class="center">
+      <amplify-authenticator> </amplify-authenticator>
+    </div>
+    <div v-if="isSignedIn">
+      <v-navigation-drawer v-model="drawer" app>
+        <v-list
+          class="d-flex flex-column justify-end align-center"
+          height="100%"
+        >
+          <v-list-item>
+            <amplify-sign-out></amplify-sign-out>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
       <v-app-bar dark app>
-        <v-app-bar-nav-icon class="font-lg"></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon
+          @click.stop="drawer = !drawer"
+          class="font-lg"
+        ></v-app-bar-nav-icon>
         <v-spacer></v-spacer>
         <v-app-bar-title class="font-lg">Easy Pantry</v-app-bar-title>
         <v-spacer></v-spacer>
@@ -10,18 +26,48 @@
       <v-main>
         <router-view />
       </v-main>
-      <amplify-sign-in></amplify-sign-in>
-    </amplify-authenticator>
+    </div>
   </v-app>
 </template>
 
 <script>
-import { components } from "aws-amplify-vue";
+import { Auth } from "aws-amplify";
+import { components, AmplifyEventBus } from "aws-amplify-vue";
 
 export default {
   name: "App",
   components: {
     ...components,
+  },
+  data: () => ({
+    isSignedIn: false,
+    drawer: null,
+  }),
+  created() {
+    this.findUser();
+
+    AmplifyEventBus.$on("authState", (info) => {
+      if (info === "signedIn") {
+        this.findUser();
+      } else {
+        this.isSignedIn = false;
+      }
+    });
+  },
+  methods: {
+    async findUser() {
+      try {
+        const user = Auth.currentAuthenticatedUser();
+
+        user.then((res) => {
+          this.isSignedIn = true;
+
+          console.log(res);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    },
   },
 };
 </script>
